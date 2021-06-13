a178bb8041af06447e1158bce2dcb8a2cc07d419
Estela Zelaya <ezelayal01@educantabria.es>
2021-06-13 09:57:06 +0200

    13/06/2021 9:57

--- php/mod/plantillasDelDiseno/header.php
+++ php/mod/plantillasDelDiseno/header.php
@@ -6,19 +6,23 @@ ob_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="../../imagenes/imgMaquetacion/favicon.png"/>
+    <link rel="icon" type="image/png" href="../../imagenes/imgMaquetacion/favicon.png" />
 
     <title><?php echo $titulo;
             session_start(); ?></title>
     <!-- CSS PROPIO -->
     <link rel="stylesheet" href="../../css/estilos.css">
-    
+
 
     <!-- JAVASCRIP PROPIO -->
     <script src="../../js/cookies.js"></script>
     <script src="../../js/validaFormularios.js"></script>
 
     <!-- BOOTSTRAP  -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -26,7 +30,10 @@ ob_start();
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
     <!-- PARA TABLAS DE ADMINISTRACION -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- BORRAR -->
@@ -85,28 +92,27 @@ ob_start();
         </nav>
 
         <div class="row cabeceraBlanca">
-            <nav class="navbar navbar-expand-md  navbar-light navLogo">
-                <div class="col-xs-10 col-sm-10 col-md-6 col-lg-6"><a class="" href="../inc/index.php">
+            <nav class="navbar navbar-expand-md  navbar-light ">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6"><a class="" href="../inc/index.php">
                         <img src="../../imagenes/imgMaquetacion/logo.png" alt="Logo" class="img-fluid">
                     </a>
                 </div>
-                <div class="col-xs-2 col-sm-0 col-md-0 col-lg-0 boton"> <button class="navbar-toggler col-xs-12 col-sm-12 col-md-0 col-lg-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="col-xs-6 col-sm-2 col-md-2 col-lg-2 sinPad">
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                 </div>
-                <div class="col-xs-0 col-sm-12 col-md-6 col-lg-6">
-                    <div class="cajabuscar col-xs-0 col-sm-12 col-md-12 col-lg-12 d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block d-lg-none d-xl-block">
-                        <form action="busqueda.php" method="post" id="buscarform" class="">
-                            <div class="input-group">
-                                <input type="text" name="buscar" id="buscar" class="form-control" placeholder="Buscar">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-default" type="submit">
-                                        <i class="glyphicon glyphicon-search"></i>
-                                    </button>
-                                </div>
+                <div class="col-xs-2 col-sm-8 col-md-4 col-lg-4 sinPad">
+                    <form action="busqueda.php" method="post" id="buscarform" class="form-inline">
+                        <div class="input-group">
+                            <input type="text" name="buscar" id="buscar" class="form-control" placeholder="Buscar">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </nav>
 
@@ -140,24 +146,6 @@ ob_start();
                             <a class="nav-link" href="contacto.php">Contacto <img src="../../imagenes/imgMaquetacion/contacto.png" alt=""></a>
                         </li>
                     </ul>
-                    <!-- <form class="form-inline my-2 my-lg-0 d-block d-sm-none">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                  </form> -->
-
-                    <div class="cajabuscar form-inline my-2 my-lg-0 d-block d-sm-none">
-                        <form method="get" id="buscarform">
-                            <div class="input-group">
-                                <input type="text" id="buscar" class="form-control" placeholder="Search">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-default" type="submit">
-                                        <i class="glyphicon glyphicon-search"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
                 </div>
             </nav>
 
